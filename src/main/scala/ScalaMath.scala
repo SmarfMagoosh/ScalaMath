ea5b90cf81fdf84390fc71c550d072fdca44c85f
Dreher <dreheren20@gcc.edu>
2023-04-08 12:54:37 -0400

    beginning to write primes and integrals

--- src/main/scala/ScalaMath.scala
+++ src/main/scala/ScalaMath.scala
@@ -1,8 +1,9 @@
+import scala.annotation.internal.Child
 import scala.util.Random
 import scala.annotation.tailrec
 
 // Helpers
-private val r = Random
+private val r: Random = Random
 private def tests: List[Double] = for _ <- (0 to 50).toList yield r.nextInt(100) * r.nextDouble()
 
 // Extensions for making Func arithmetic compatible with integers and doubles
@@ -27,9 +28,9 @@ private abstract class Func {
 
   def of: Double => Double
 
-  def prime: Func = this // TODO make abstract
+  def prime: Func
 
-  def integral: Func = this // TODO make abstract
+  def integral: Func
 
   def simplify: Func = this
 
@@ -144,13 +145,17 @@ private abstract class Func {
 
 private class Erf extends Func {
   def of: Double => Double = _ => Double.NaN
+  def prime: Func = this
+  def integral: Func = this
 }
 
 private class Sum(f: Func, g: Func) extends Func {
   val _1: Func = f
   val _2: Func = g
 
-  def of: Double => Double = (x: Double) => f(x) + g(x)
+  def of: Double => Double = (d: Double) => f(d) + g(d)
+  def prime: Func = Sum(f.prime, g.prime).simplify
+  def integral: Func = Sum(f.integral, g.integral).simplify
 
   override def simplify: Func = if f *= g then Scalar(f.scale + g.scale, f.asScalar._2).simplify else this
 }
@@ -159,7 +164,9 @@ private class Product(f: Func, g: Func) extends Func {
   val _1: Func = f
   val _2: Func = g
 
-  def of: Double => Double = (x: Double) => f(x) * g(x)
+  def of: Double => Double = (d: Double) => f(d) * g(d)
+  def prime: Func = Sum(Product(f.prime, g).simplify, Product(f, g.prime).simplify).simplify
+  def integral: Func = Erf() // TODO try to solve
 
   override def simplify: Func = {
     if f.isPoly && g.isPoly then Scalar(f.scale * g.scale, x**(f.as[Poly]._1 + g.as[Poly]._1)).simplify
@@ -177,18 +184,31 @@ private class Comp(f: Func, g: Func) extends Func {
   val _1: Func = f
   val _2: Func = g
 
-  def of: Double => Double = (x: Double) => f(g(x))
+  def of: Double => Double = (d: Double) => f(g(d))
+  def prime: Func = Product(Comp(f.prime, g), g.prime)
+  def integral: Func = Erf() // TODO solve
 
-  override def simplify: Func = super.simplify // TODO implement
+  override def simplify: Func = {
+    if f.isPoly && g.isPoly then Scalar(f.scale * Math.pow(g.scale, f.as[Poly]._1), Poly(f.as[Poly]._1 * g.as[Poly]._1))
+    else if f.isLog then
+      if g.isPoly then Scalar(f.scale * g.as[Poly]._1, Log(f.as[Log]._1))
+      else if g.isExp then Scalar(f.scale * Log(f.as[Log]._1)(g.as[Exp]._1), x)
+      else if g.isTetr then Product(f.scale * g.as[Tetr]._2, Log(f.as[Log]._1)(g.as[Tetr]._1))
+      else super.simplify
+    else if f.isExp && g.isLog then x ** (1 / ln(g.as[Log]._1))
+    else super.simplify
+  }
 }
 
 private class Tetr(f: Func, g: Func) extends Func {
   val _1: Func = f
   val _2: Func = g
 
-  def of: Double => Double = (x: Double) => Math.pow(f(x), g(x))
+  def of: Double => Double = (d: Double) => Math.pow(f(d), g(d))
+  def prime: Func = Erf() // TODO solve
+  def integral: Func = Erf() // TODO solve
 
-  override def simplify: Func = super.simplify // TODO implement
+  override def simplify: Func = if f *= 1 then f(1) ** g else if g *= 1 then f ** g(1) else super.simplify
 }
 
 private class Scalar(scalar: Double, f: Func) extends Func {
@@ -197,6 +217,10 @@ private class Scalar(scalar: Double, f: Func) extends Func {
   val _2: Func = f
 
   def of: Double => Double = (x: Double) => scalar * (f of x)
+
+  def prime: Func = scalar * f.prime
+
+  def integral: Func = scalar * f.integral
 
   override def simplify: Func = if scalar == 1 then f else super.simplify
 
@@ -209,120 +233,214 @@ private class Constant(constant: Double) extends Func {
   val _1: Double = constant
 
   def of: Double => Double = _ => constant
+
+  def prime: Func = Constant(0)
+
+  def integral: Func = constant * x
 }
 
 private class Poly(exp: Double) extends Func {
   val _1: Double = exp
 
   def of: Double => Double = (x: Double) => Math.pow(x, exp)
+
+  def prime: Func = exp * Poly(exp - 1)
+
+  def integral: Func = (1.0 / (exp + 1)) * Poly(exp + 1)
 }
 
 private class Exp(base: Double) extends Func {
   val _1: Double = base
 
   def of: Double => Double = (x: Double) => Math.pow(base, x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class Log(base: Double) extends Func {
   val _1: Double = base
 
   def of: Double => Double = (x: Double) => Math.log(x) / Math.log(base)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class Sine extends Func {
   def of: Double => Double = (x: Double) => Math.sin(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class Cosine extends Func {
   def of: Double => Double = (x: Double) => Math.cos(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class Tangent extends Func {
   def of: Double => Double = (x: Double) => Math.tan(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class Secant extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.cos(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class Cosecant extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.sin(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class Cotangent extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.tan(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
+
 }
 
 private class ArcSine extends Func {
   def of: Double => Double = (x: Double) => Math.asin(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class ArcCosine extends Func {
   def of: Double => Double = (x: Double) => Math.acos(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class ArcTangent extends Func {
   def of: Double => Double = (x: Double) => Math.atan(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class ArcSecant extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.acos(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class ArcCosecant extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.asin(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class ArcCotangent extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.atan(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class HyperbolicSine extends Func {
   def of: Double => Double = (x: Double) => Math.sinh(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class HyperbolicCosine extends Func {
   def of: Double => Double = (x: Double) => Math.cosh(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class HyperbolicTangent extends Func {
   def of: Double => Double = (x: Double) => Math.tanh(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class HyperbolicSecant extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.cosh(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class HyperbolicCosecant extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.sinh(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 private class HyperbolicCotangent extends Func {
   def of: Double => Double = (x: Double) => 1.0 / Math.tanh(x)
+
+  def prime: Func = Erf() // TODO solve
+
+  def integral: Func = Erf() // TODO solve
 }
 
 val ln: Log = Log(Math.E)
 val lg: Log = Log(2)
 val log: Double => Log = (d: Double) => Log(d)
+
 val x = Poly(1)
+
 val sin = Sine()
 val cos = Cosine()
 val tan = Tangent()
 val sec = Secant()
 val csc = Cosecant()
 val cot = Cotangent()
+
 val arcsin = ArcSine()
 val arccos = ArcCosine()
 val arctan = ArcTangent()
 val arcsec = ArcSecant()
 val arccsc = ArcCosecant()
 val arccot = ArcCotangent()
+
 val sinh = HyperbolicSine()
 val cosh = HyperbolicCosine()
 val tanh = HyperbolicTangent()
 val sech = HyperbolicSecant()
 val csch = HyperbolicCosecant()
 val coth = HyperbolicCotangent()
+
 val e = Math.E
 val pi = Math.PI
 val phi = (1 + Math.sqrt(5)) / 2
@@ -333,10 +451,3 @@ val tau = Math.TAU
 // Probability
 
 // solve linear equations
-
-@main def main(): Unit = {
-  val f: Func = (2*(x**0.5))*(0.25 * (x**3))
-  println(f.getClass)
-  println(f.funcType)
-  println(f of pi)
-}
